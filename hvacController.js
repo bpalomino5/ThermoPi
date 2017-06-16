d1f3ca13e1a7edee3af7a5fdfe1d151b73544e59
Brandon Palomino <12bpalomino@gmail.com>
2017-06-16 16:18:51 -0700

    compressor air flow settings added, and better variable naming

--- hvacController.js
+++ hvacController.js
@@ -28,7 +28,7 @@ var BLYNK_RED = "#D3435C"
 var PIN_TEMPERATURE = 4
 var PIN_FAN = 17
 var PIN_COMPRESSOR = 18
-var PIN_COOL = 27
+var PIN_FLOW = 27
 
 var RELAY_ON = 0        // GPIO.LOW 0
 var RELAY_OFF = 1    // GPIO.HIGH 1
@@ -43,17 +43,17 @@ var sensor = require('node-dht-sensor');
 var Gpio = require('pigpio').Gpio,
   FAN = new Gpio(PIN_FAN, {mode: Gpio.OUTPUT}),
   COMPRESSOR = new Gpio(PIN_COMPRESSOR, {mode: Gpio.OUTPUT}),
-  COOL = new Gpio(PIN_COOL, {mode: Gpio.OUTPUT});
+  FLOW = new Gpio(PIN_FLOW, {mode: Gpio.OUTPUT});
 
 //Setup blynk
 var Blynk = require('blynk-library');
 var blynk = new Blynk.Blynk(blynkToken);
 
 //Setup Virtual Pins
-var v0 = new blynk.VirtualPin(0);
-var v1 = new blynk.VirtualPin(1);
+var v0 = new blynk.VirtualPin(0);   // Power
+var v1 = new blynk.VirtualPin(1);   // Hot/Cold
 // var v2 = new blynk.VirtualPin(2)
-var v4 = new blynk.VirtualPin(4);
+var v4 = new blynk.VirtualPin(4);   // Temperature Scale
 
 blynk.on('connect', function() { console.log("Blynk ready.\n\nSettings Log:"); });
 blynk.on('disconnect', function() { console.log("DISCONNECT"); });
@@ -82,10 +82,12 @@ v1.on('write', function(param) {
     if (param[0] === '1') {
       blynk.setProperty(1,"color",BLYNK_RED);
       console.log("*** PUMP: HOT ***");
+      setCompHot()
 
     } else { 
       blynk.setProperty(1,"color",BLYNK_BLUE);
       console.log("*** PUMP: COLD ***");
+      setCompCold()
     }
 });
 
@@ -94,15 +96,24 @@ v4.on('read', function(val) {
   v4.write(getTemp());
 });
 
+/// Functions for Power
 function PowerOn() {
   FAN.digitalWrite(RELAY_ON);
   COMPRESSOR.digitalWrite(RELAY_ON);
-  COOL.digitalWrite(RELAY_ON);
+  FLOW.digitalWrite(RELAY_ON);
 }
 
 function PowerOff() {
   FAN.digitalWrite(RELAY_OFF);
   COMPRESSOR.digitalWrite(RELAY_OFF);
-  COOL.digitalWrite(RELAY_OFF);
+  FLOW.digitalWrite(RELAY_OFF);
 }
 
+/// Functions for Compressor Air Flow
+function setCompHot() {     // R+G+Y active sets hot air
+  FLOW.digitalWrite(RELAY_OFF);
+}
+
+function setCompCold() {    // R+G+Y+O active sets cold air
+  FLOW.digitalWrite(RELAY_ON); 
+}
